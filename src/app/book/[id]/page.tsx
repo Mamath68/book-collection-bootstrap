768cc26eb49e78a71d8166968c501983606b0ab9
mathieu stamm <mathieu.stamm@gmail.com>
2024-12-15 10:48:28 +0100

    modification du détail d'un livre

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -4,16 +4,16 @@ import path from "path";
 import {promises as fs} from "fs";
 
 // Fonction pour récupérer les données du fichier JSON
-async function fetchBookData(id: string) {
+async function fetchBookData(id: number) {
     const filePath = path.join(process.cwd(), "public", "books.json");
     const data = await fs.readFile(filePath, "utf-8");
     const books = JSON.parse(data);
 
-    return books.find((book: { id: number }) => book.id === parseInt(id));
+    return books.find((book: { id: number }) => book.id === parseInt(String(id)));
 }
 
 // Génération dynamique des métadonnées
-export async function generateMetadata({params}: { params: { id: string } }): Promise<Metadata> {
+export async function generateMetadata({params}: { params: { id: number } }): Promise<Metadata> {
     const book = await fetchBookData(params.id);
 
     if (!book) {
@@ -30,7 +30,7 @@ export async function generateMetadata({params}: { params: { id: string } }): Pr
 }
 
 // Composant principal de la page
-export default async function BookPage({params}: { params: { id: string } }) {
+export default async function BookPage({params}: { params: { id: number } }) {
     const book = await fetchBookData(params.id);
 
     if (!book) {
