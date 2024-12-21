e30b7255ea36703e69052e757247bced2dd859c8
mathieu stamm <mathieu.stamm@gmail.com>
2024-12-21 15:38:42 +0100

    Personalisation de la page d'accueil

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 
 import type {Metadata} from 'next'
+import {Container} from "react-bootstrap";
 
 export const metadata: Metadata = {
     title: 'Accueil',
@@ -9,6 +10,14 @@ export const metadata: Metadata = {
 
 export default function Home() {
     return (
-        <Link href="/book">Vers les livres</Link>
+        <Container className="text-center">
+            <h1>Bonjours à tous et Bienvenue sur mon site</h1>
+            <h2>Ceci est ma nouvelle et dernière version de ma collection de livres.</h2>
+            <h3>Je n&#39;ai pas trouvé les informations concernant les BD, où alors
+                les sources ne sont pas forcement fiable ou complète. Vous ne verrez donc que les Mangas.</h3>
+            <h4>Pour plus d&#39;infos concernant les BD, faudra me demander des détails.</h4>
+            <h5>Je vous souhaite bonne <Link href="/book">navigation</Link></h5>
+        </Container>
     );
 }
+
