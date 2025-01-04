fe1c079b9a32a89a63f1a66e9895a39e6ed85378
mathieu stamm <mathieu.stamm@gmail.com>
2025-01-04 19:05:30 +0100

    ajout d'un aria-hidden

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({children}: Readonly<{
     return (
         <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <header>
+        <header aria-hidden={true}>
             <Navbar/>
         </header>
         <div className={styles.page}>
